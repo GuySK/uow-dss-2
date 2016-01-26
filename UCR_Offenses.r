211b3496f3afd1e94642aac588ead2cceccc81b9
GuySK <guillermo.sorini@gmail.com>
2016-01-26 19:26:49 -0300

    Initial load

---
+++ UCR_Offenses.r
@@ -0,0 +1,12 @@
+
+# Offenses against Persons
+person <- c('Assault','Homicide','Kidnapping/Abduction','Sex , Forcible','Sex , Nonforcible')
+
+# Offenses against Property
+property <- c('Arson','Bribery','Burglary/Breaking & Entering','Counterfeiting/Forgery','Destruction/Damage/Vandalism of Property','Embezzlement','Extortion/Blackmail','Fraud ','Larceny/Theft ','Motor Vehicle Theft','Stolen Property ','Robbery','Bad Checks')
+
+# Offenses against Society
+society <- c('Drug/Narcotic ','Gambling ','Pornography/Obscene Material','Weapon Law Violations','Curfew/Loitering/Vagrancy','Disorderly Conduct','Driving Under the Influence','Drunkenness','Family , Nonviolent','Liquor Law Violations','Peeping Tom','Trespass of Real Property')
+
+# Not a Crime
+NaC <- c('Runaway')
